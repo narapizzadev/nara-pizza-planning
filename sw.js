cd62861c8ec8b80869d6433cac88c1a15e3ae0e2
narapizzadev <narapizzadev@gmail.com>
2026-08-01 11:18:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    CACHE_VERSION. C'est ce qui force les telephones a prendre la nouvelle
    version au lieu de servir l'ancienne indefiniment. */
 
-const CACHE_VERSION = "2026-07-31-b";
+const CACHE_VERSION = "2026-07-31-c";
 const CACHE = `journia-${CACHE_VERSION}`;
 
 const SHELL = [
